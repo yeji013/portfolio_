4c269fda55bdec3e1d777927951df9ec283882e7
yeji013 <108514729+yeji013@users.noreply.github.com>
2022-08-04 18:35:49 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+new fullpage(
+    '#wrap', 
+    {
+    	autoScrolling:true ,
+        anchors:['1page', '2page','3page','4page' ,'5page','6page','7page','8page'],
+        navigation: true,
+        afterLoad: function(origin, destination, direction, trigger){
+         
+       
+        }        
+    }
+);
+
